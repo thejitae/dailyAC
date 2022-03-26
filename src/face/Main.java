8cdc78fe7fcdb457a4ab9c8fb119d295d853cf5b
JITAE <thejitae@gmail.com>
2022-03-27 00:53:56 +0900

    5537 ㅠㅠㅠ 졸았어... 브론즈5로 오늘 땡

---
+++ src/face/Main.java
@@ -0,0 +1,15 @@
+package face;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public interface Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        //int N = Integer.parseInt(st.nextToken());
+    }
+}
