82c03d6e5137205bfbeb276cc99605f7e8bf2aa6
JITAE <thejitae@gmail.com>
2022-03-27 23:59:43 +0900

    9012 cheat

--- src/face/Main.java
+++ src/face/Main.java
@@ -9,6 +9,7 @@ public interface Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //StringTokenizer st = new StringTokenizer(br.readLine());
+        //StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         //int N = Integer.parseInt(st.nextToken());
     }
