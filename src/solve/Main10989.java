494274bb3b85c9a74b9de99ae0e745de0956da4b
JITAE <thejitae@gmail.com>
2022-04-05 23:12:43 +0900

    10989 solved

---
+++ src/solve/Main10989.java
@@ -0,0 +1,28 @@
+package solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[Integer.parseInt(br.readLine())];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
