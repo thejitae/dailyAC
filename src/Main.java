00d1b7a7dd9e892cb10d12231059aac7096c7d07
JITAE_nexgrid <thejitae@gmail.com>
2022-04-14 18:48:28 +0900

    1929 cheat

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    //1929 cheat... 다시 풀기 ㅠㅠ
+    //1929 cheat... 다시 풀기 ㅠ
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
