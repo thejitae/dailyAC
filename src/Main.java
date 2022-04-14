92bb20efdeb564392ebd17ec5f8eafcccb89eaa7
JITAE_nexgrid <thejitae@gmail.com>
2022-04-14 18:46:45 +0900

    1929 cheat

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    //1929
+    //1929 cheat... 다시 풀기 ㅠㅠㅠㅠ
     public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        //3 4 5 6 7 8 9 10 11 12 13 14 15 16
-        for (int i = M; i <= N; i++) {
+        boolean[] prime = new boolean[N + 1];
 
-            //1 이하 버리기
-            if (i < 2) {
-                continue;
+        for(int i = 2; i <= N; i++) {
+            if(prime[i]) continue;
+
+            if(i >= M) sb.append(i).append('\n');
+
+            for(int j = i + i; j <= N; j += i) {
+                prime[j] = true;
             }
-
-            //2 넣기
-            if (i == 2) {
-                sb.append(i).append('\n');
-            }
-
-            //2가 아닌 짝수 버리기
-            if (i != 2 && i % 2 == 0) {
-                continue;
-            }
-
-            //2부터 나누기 시작해서 나머지가 0이 되는 게 있으면 버리기
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    continue;
-                }
-            }
-
-            sb.append(i).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    public static void isPrime(int number) {
-
-    }
 }
