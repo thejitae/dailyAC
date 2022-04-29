9108a557d14327f2f803f97377e059d4d170c753
JITAE_nexgrid <thejitae@gmail.com>
2022-04-29 18:47:58 +0900

    2675 solve

---
+++ src/solve/Main2675.java
@@ -0,0 +1,26 @@
+package solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    sb.append(S.charAt(j));
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
