d1879092a30ed66e22acb2783c2d94a63c40fb6a
JITAE_nexgrid <thejitae@gmail.com>
2022-05-06 18:33:25 +0900

    1292 on going

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    //1292
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        //A번째 숫자부터 B번째 숫자까지 합
+        //3 7
+        //1223334444555556666667777777
+        //1234567890
+        //2+3+3+3+4
+        //15
+
+
+
+
+
+    }
+}
--- src/cheat/Main1181.java
+++ src/cheat/Main1181.java
@@ -1,3 +1,5 @@
+package cheat;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
