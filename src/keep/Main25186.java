3c938588f7b89be8b8c7dcf3afd1c39438fe7f8c
JITAE <thejitae@gmail.com>
2022-05-21 17:41:33 +0900

    1157 solve

--- src/keep/Main25186.java
+++ src/keep/Main25186.java
@@ -1,9 +1,11 @@
+package keep;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Saturday {
+public class Main25186 {
     //25186
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +14,7 @@ public class Saturday {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int[] arr = new int[N];
+        long[] arr = new long[N];
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -22,6 +24,9 @@ public class Saturday {
 
         for (int i = 0; i < N; i++) {
             if (i == 0) {
+                if (arr[i] == arr[N-1]) {
+                    isHappy = false;
+                }
                 continue;
             }
             if (arr[i] == arr[i-1]) {
--- src/solve/Main1157.java
+++ src/solve/Main1157.java
@@ -1,8 +1,10 @@
+package solve;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Sunday {
+public class Main1157 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine().toUpperCase();
