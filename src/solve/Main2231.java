4d6152af5047b87eba48ee32d86643d0ed40c8b8
JITAE_nexgrid <thejitae@gmail.com>
2022-05-25 18:08:01 +0900

    2231 solve

---
+++ src/solve/Main2231.java
@@ -0,0 +1,28 @@
+package solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =Integer.parseInt(br.readLine());
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == N) {
+                res = i;
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+}
