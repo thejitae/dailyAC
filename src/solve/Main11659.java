17ff3f3df93a03aec6c3797fec14f5a95fdc05d5
JITAE <thejitae@gmail.com>
2022-05-28 16:31:05 +0900

    11659 solve

--- src/solve/Main11659.java
+++ src/solve/Main11659.java
@@ -1,10 +1,11 @@
+package solve;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    //11659
+public class Main11659 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -13,11 +14,12 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] arr = new int[N+1];
+        arr[0] = 0;
 
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < M; i++) {
@@ -25,9 +27,8 @@ public class Main {
             st = new StringTokenizer(br.readLine()," ");
             int first = Integer.parseInt(st.nextToken());
             int last = Integer.parseInt(st.nextToken());
-            for (int j = first-1; j <= last-1; j++) {
-                sum += arr[j];
-            }
+
+            sum = arr[last] - arr[first-1];
             sb.append(sum).append('\n');
         }
 
