e730ec887eded72651c7a5d4fd3bf04fadb64529
JITAE_nexgrid <thejitae@gmail.com>
2022-06-02 17:21:09 +0900

    C

---
+++ src/cheat/Main11279.java
@@ -0,0 +1,28 @@
+package cheat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                sb.append(pq.size() == 0 ? 0 : pq.poll()).append('\n');
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
