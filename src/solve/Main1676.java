188f539e030fde23897b67f0ee53d35dd11056f5
JITAE <thejitae@gmail.com>
2022-07-04 21:31:02 +0900

    1676 solve

---
+++ src/solve/Main1676.java
@@ -0,0 +1,18 @@
+package solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (N >= 5) {
+            count += N/5;
+            N /= 5;
+        }
+        System.out.println(count);
+    }
+}
